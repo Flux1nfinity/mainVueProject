f3c57a09cded9de3824c36023084b9592529819c
Julian <juliandhernandez2000@gmail.com>
2023-09-21 23:25:47 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="32" height="32" />
+    <a href="#">About</a>
+    <a href="#">Catalog</a>
+    <a href="#">Community</a>
+    <a href="#">Support</a>
+    <a href="#">Donate</a>
+    <img alt="user logo" class="user" src="./assets/user-solid.svg" width="24" height="24" />
+  </header>
+</template>
+
+<style scoped>
+
+.logo {
+  display: block;
+  margin: 0;
+} 
+
+header {
+  width: 100vw;
+  height: 3rem;
+  background-color: #181818;
+  display: flex;
+  box-shadow: 0 10px 6px -6px #292929;
+  overflow: hidden;
+}
+header a{
+  padding: 0.55rem 2rem;
+  font-family: calibri;
+  font-size: 18px;
+  font-weight: lighter;
+}
+
+.logo {
+  align-self: center;
+  margin: 0 5rem 0 5rem;
+}
+.user {
+  transform: translateX(50vw);
+  align-self: center;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="nav-container">
+    <div class="nav-pages">
+      <a href="#">About</a>
+      <a href="#">Catalog</a>
+      <a href="#">Community</a>
+      <a href="#">Support</a>
+      <a href="#">Donate</a>
+    </div>
+  </div>
+</template>
+
+<style>
+.nav-container {
+  width: 100vw;
+  height: 4rem;
+  overflow: hidden;
+  background-color: #66686e;
+}
+.nav-pages {
+  display: flex;
+  justify-content: flex-start;
+}
+.nav-pages a {
+  font-family: calibri;
+  font-weight: lighter;
+  text-decoration: none;
+  color: #eeeeee;
+}
+</style>
